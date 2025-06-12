326da7149a5acd922f4bf3981c4128e72b4d0635
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-06-12 04:54:27 +0300

    init posts route, install dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
-require("dotenv/lib/main").config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const postsRouter = require("./routers/postsRouter");
+app.use("/posts", postsRouter);
 
 app.listen(PORT, () => {
   console.log(`API is online at port ${PORT}`);
