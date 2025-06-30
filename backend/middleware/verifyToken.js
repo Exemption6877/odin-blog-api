d844f12c01a0871ed810242d1d99d49c92b13d6b
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-06-30 06:23:17 +0300

    Create verifyToken, hold user info in token

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    try {
+      const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
+      req.user = decoded;
+
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+module.exports = { verifyToken };
