a146dd2b31870c21e86b9feae58bb18082ed9d9b
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-04 05:44:45 +0300

    Render comments in post details

--- frontend/main/src/components/Main/components/PostDetails.jsx
+++ frontend/main/src/components/Main/components/PostDetails.jsx
@@ -4,16 +4,27 @@ import useFetch from "../../../../hooks/useFetch";
 function PostDetails() {
   const API_URL = import.meta.env.VITE_API_URL;
   const { postId } = useParams();
-  const { error, loading, data } = useFetch(`${API_URL}/posts/${postId}`);
+  const post = useFetch(`${API_URL}/posts/${postId}`);
+  const comments = useFetch(`${API_URL}/posts/${postId}/comments`);
+  if (post.error) return <div>Error: {post.error}</div>;
+  if (post.loading) return <div>Loading...</div>;
+  if (!post.data) return <div>Post Not Found.</div>;
 
-  if (error) return <div>Error: {error}</div>;
-  if (loading) return <div>Loading...</div>;
-  if (!data) return <div>Post Not Found.</div>;
+  if (!comments.data) return <div>Comment Not Found.</div>;
+  //
 
   return (
-    <div>
-      <h2>{data.title}</h2>
-      <p>{data.content}</p>
+    <div key={post.data.id}>
+      <h2>{post.data.title}</h2>
+      <p>{post.data.content}</p>
+
+      <div className="comments">
+        {comments.data.map((comment) => (
+          <div key={comment.id}>
+            <p>{comment.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
