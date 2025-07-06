e6dd7ef77f71efecc1770e89e1ca21326c1879f9
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-06 03:19:20 +0300

    Fix centering issues

--- frontend/main/src/components/Auth/Auth.jsx
+++ frontend/main/src/components/Auth/Auth.jsx
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
+import styles from "./Auth.module.css";
 
 function Auth() {
-  return <Outlet />;
+  return (
+    <div className={styles.authWrapper}>
+      <Outlet />
+    </div>
+  );
 }
 
 export default Auth;
