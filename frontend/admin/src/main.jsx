d288293ad8b77385454b197d0d9aa867a88ae2b5
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-07 05:46:25 +0300

    Create protected route

--- frontend/admin/src/main.jsx
+++ frontend/admin/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import routes from "./routes.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AuthProvider from "./provider/AuthProvider.jsx";
 import App from "./App.jsx";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>
 );
--- frontend/admin/src/routes.jsx
+++ frontend/admin/src/routes.jsx
@@ -1,3 +1,16 @@
-// import App from "../../admin/src/App";
-// const routes = [];
-// export default routes;
+import App from "../../admin/src/App";
+import Login from "./components/Auth/Login";
+import AuthRoute from "../../admin/src/ProtectedRoute";
+
+const routes = [
+  { path: "/", element: <Login /> },
+  {
+    path: "/posts",
+    element: (
+      <AuthRoute>
+        <App />
+      </AuthRoute>
+    ),
+  },
+];
+export default routes;
