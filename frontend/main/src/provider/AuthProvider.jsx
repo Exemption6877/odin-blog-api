0d87627d87ddb172cbeac4e7a3b50f79b97de5a1
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-09 21:27:00 +0300

    Another fix for import

--- frontend/main/src/provider/AuthProvider.jsx
+++ frontend/main/src/provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import AuthContext from "../context/AuthContext";
+import AuthContext from "../context/authContext";
 
 export default function AuthProvider({ children }) {
   const [token, setToken] = useState(() => localStorage.getItem("token"));
